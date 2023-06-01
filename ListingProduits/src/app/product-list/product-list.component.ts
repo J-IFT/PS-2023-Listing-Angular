264d5f540da93d4825d2c77d55fc5b2511e9db2f
J-IFT <juliette.infanti@gmail.com>
2023-06-01 15:56:08 +0200

    ✨ on peut ajouter un produit (bug voir discord)

--- ListingProduits/src/app/product-list/product-list.component.ts
+++ ListingProduits/src/app/product-list/product-list.component.ts
@@ -14,14 +14,55 @@ export class ProductListComponent implements OnInit {
   ];
 
   selectedProduct: Product | null = null;
-  isEditing = false; // Ajoutez cette ligne pour définir la propriété isEditing
+  isEditing = false;
+  newProduct: Product = {
+    id: '',
+    nom: '',
+    texture: '',
+    grammage: '',
+    couleur: ''
+  };
 
   ngOnInit(): void {
-    // Sélectionner le premier produit de la liste
     this.selectedProduct = this.products[0];
   }
 
   selectProduct(product: Product): void {
     this.selectedProduct = product;
+  }
+
+  getMaxProductId(): number {
+    let maxId = 0;
+    for (const product of this.products) {
+      const productId = parseInt(product.id, 10);
+      if (productId > maxId) {
+        maxId = productId;
+      }
+    }
+    return maxId;
+  }
+
+  showProductForm(): void {
+    if (!this.isEditing) {
+      this.selectedProduct = null;
+      this.isEditing = true;
+    }
+  }
+
+  addProduct(): void {
+    const newProductId = this.getMaxProductId() + 1;
+    const newProduct: Product = {
+      id: newProductId.toString(),
+      nom: this.newProduct.nom,
+      texture: this.newProduct.texture,
+      grammage: this.newProduct.grammage,
+      couleur: this.newProduct.couleur
+    };
+    this.products.push(newProduct);
+    this.isEditing = false;
+  }
+
+  cancelAdd(): void {
+    this.isEditing = false;
   }
 }
